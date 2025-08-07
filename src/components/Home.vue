97ec3ad8e21d65eaa4f2e0b20e3659ba704e29d2
小白 <121837289+cunzhong@users.noreply.github.com>
2025-08-07 18:06:16 +0800

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,8 +6,8 @@
         <span class="status-ball"></span>
       </div>
       <div class="user-name" v-motion-slide-left>
-        <h1>Hi,</h1>
-        <h1>I'm <span class="name-style">{{ userName }}</span></h1>
+        <h1>Hi!</h1>
+        <h1>This is <span class="name-style">{{ userName }}</span></h1>
       </div>
     </div>
     <div class="description">
